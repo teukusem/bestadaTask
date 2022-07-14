c66f9905dfd7b1ece336ca7a4b0aff95e1c9f5a3
teukusem <syamteuku4@gmail.com>
2022-07-15 03:25:44 +0700

    first commit

---
+++ arrayCheck.js
@@ -0,0 +1,6 @@
+let arrayCheck = [3, 7, 1, 2, 6, 7, 8, 9, 12, 5, 3, 12];
+arrayCheck.sort(function (a, b) {
+  return b - a;
+});
+
+console.log(arrayCheck);
---
+++ bestadaCheck.js
@@ -0,0 +1,6 @@
+const input = 100;
+for (let i = 0; i <= input; i++) {
+  if (i % 7 == 0) console.log("Bestada");
+  else if (i === input) console.log("Sukses");
+  else console.log(i);
+}
